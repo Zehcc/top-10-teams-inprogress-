68d5134fa6587b9527fc35d00ae73cb4080e4c1c
Tomás Sánchez <Tomassanchezmedina92@gmail.com>
2022-03-22 17:22:28 +0100

    chore: optimizing code

--- front/app.js
+++ front/app.js
@@ -1,27 +1,23 @@
 const urlTeams = 'http://localhost:8000/api/teams'
+let teams = [];
+let players = [];
 window.onload = () => {
     init();
 }
 
 const init = async () => {
     document.querySelector('main').innerHTML = '';
-    const teams = await getTeams();
-    paintTeams(teams);
+    await getTeams();
+    await paintTeams();
 }
 
 const getTeams = async() => {
     const result = await fetch(urlTeams);
     const resultjs = await result.json();
-    return resultjs
+    teams = [...resultjs]
 }
 
-const getTeam = async (id) => {
-    const result = await fetch(`${urlTeams}/${id}`);
-    const resultjs = await result.json();
-    return resultjs
-}
-
-const paintTeams = (teams) => {
+const paintTeams = () => {
     const teamsDiv = document.createElement('div');
     const teamsList = document.createElement('ul');
     teamsList.className = 'teamsList';
@@ -37,14 +33,17 @@ const paintTeams = (teams) => {
 }
 
 const paintTeam = async (id) => {
-    const team = await getTeam(id);
-    const main = document.querySelector('main');
-    main.innerHTML = '';
-    const teamDiv = document.createElement('div');
-    teamDiv.innerHTML = `<div class="teamLogo"><img src="${team.img}" alt="${team.name}"></div class="playersDiv"><div><ul class="playersList"></ul></div>`;
-    main.appendChild(teamDiv);
-    const playersList=document.querySelector('.playersList');
-    for (const player of team.players) {
-        playersList.innerHTML += `<li class ="playersItem"><div class="playerImg"><img src="${player.img}" alt="${player.name}"></div><div class="playerText"><h2>${player.name}</h2><p>Edad: ${player.age}</p><p>${player.nationality}<p></div></li>`;
+    for (const team of teams) {
+        if (team._id == id) {
+            const main = document.querySelector('main');
+            main.innerHTML = '';
+            const teamDiv = document.createElement('div');
+            teamDiv.innerHTML = `<div class="teamLogo"><img src="${team.img}" alt="${team.name}"></div class="playersDiv"><div><ul class="playersList"></ul></div>`;
+            main.appendChild(teamDiv);
+            const playersList=document.querySelector('.playersList');
+            for (const player of team.players) {
+                playersList.innerHTML += `<li class ="playersItem"><div class="playerImg"><img src="${player.img}" alt="${player.name}"></div><div class="playerText"><h2>${player.name}</h2><p>Edad: ${player.age}</p><p>${player.nationality}<p></div></li>`;
+            }
+        }
     }
 }
--- server/src/api/teams/teams.controller.js
+++ server/src/api/teams/teams.controller.js
@@ -7,7 +7,7 @@ const getAll = async (req, res, next) => {
         const teams = await Team.find().populate('players');
         res.status(200).json(teams);
     } catch (error) {
-        return next(error)
+        return next(setError(404, 'No se han podido recuperar los equipos'))
     }
 }
 
@@ -17,7 +17,7 @@ const getOne = async (req, res, next) => {
         const team = await Team.findById(id).populate('players');
         res.status(200).json(team);
     } catch (error) {
-        return next(setError(404, 'No se ha podido recuperar el usuario'))
+        return next(setError(404, 'No se ha podido recuperar el equipo'))
     }
 }
 
@@ -32,7 +32,7 @@ const postOne = async (req, res, next) => {
         const teamDB = await team.save();
         return res.status(201).json(teamDB)
     } catch (error) {
-        return next(setError(404, 'No se ha podido crear el usuario'))
+        return next(setError(404, 'No se ha podido crear el equipo'))
     }
 }
 
@@ -49,7 +49,7 @@ const patchOne = async (req, res, next) => {
         const updateTeam = await Team.findByIdAndUpdate(id, team);
         return res.status(200).json(updateTeam);
     } catch (error) {
-        return next(setError(404, 'No se ha podido modificar el usuario'));
+        return next(setError(404, 'No se ha podido modificar el equipo'));
     }
 }
 
@@ -60,7 +60,7 @@ const deleteOne = async (req, res, next) => {
         if (team.img) deleteFile(team.img)
         return res.status(200).json(team);
     } catch (error) {
-        return next(setError(404, 'No se ha podido borrar el usuario'));
+        return next(setError(404, 'No se ha podido borrar el equipo'));
     }
 }
 
